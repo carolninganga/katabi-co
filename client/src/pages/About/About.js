235230e6e399f369c2130dc5d7fae627c6cae6f6
wuyaheng <wuyaheng2016@gmail.com>
2020-08-09 18:04:27 -0400

    centered the team section

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -12,10 +12,10 @@ const About = () => {
 
           <div className="row mt-3">
     
-           <h5 className="mt-3" style={{margin: "0 auto", color: "white"}}>Katabi enables close collaboration through a WebVR experience!</h5>
+           <h5 className="mt-3 mb-2" style={{margin: "0 auto", color: "white"}}>Katabi enables close collaboration through a WebVR experience!</h5>
           </div>
           <div className="row justify-content-center">
-            <img width="60%" src="https://cdn.glitch.com/c0dcb651-5b19-4756-a6e6-25b3982ca367%2Floft-lake.gif?v=1597009278337" alt="vr room demo"/>
+            <img width="50%" src="https://cdn.glitch.com/c0dcb651-5b19-4756-a6e6-25b3982ca367%2Floft-lake.gif?v=1597009278337" alt="vr room demo"/>
           </div>
           <div className="row">
           <Team />
